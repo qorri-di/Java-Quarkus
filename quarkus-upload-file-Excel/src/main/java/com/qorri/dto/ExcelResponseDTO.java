b06a5528a6cd45a12164ea16691df059e681b605
MrR007 <qorri24j@gmail.com>
2024-06-28 19:44:32 +0700

    fixing Excel Response DTO

--- quarkus-upload-file-Excel/src/main/java/com/qorri/dto/ExcelResponseDTO.java
+++ quarkus-upload-file-Excel/src/main/java/com/qorri/dto/ExcelResponseDTO.java
@@ -1,4 +1,40 @@
 package com.qorri.dto;
 
 public class ExcelResponseDTO {
+    private Integer id;
+    private String name;
+    private String gender;
+    private String country;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
