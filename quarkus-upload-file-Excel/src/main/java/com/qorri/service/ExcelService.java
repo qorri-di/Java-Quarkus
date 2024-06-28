9fd440469797be406e5a677e75066904f85880e1
MrR007 <qorri24j@gmail.com>
2024-06-28 19:44:11 +0700

    fixing Excel Service

--- quarkus-upload-file-Excel/src/main/java/com/qorri/service/ExcelService.java
+++ quarkus-upload-file-Excel/src/main/java/com/qorri/service/ExcelService.java
@@ -1,4 +1,38 @@
 package com.qorri.service;
 
+import com.qorri.dto.*;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
+import java.io.InputStream;
+import java.util.*;
+
+@ApplicationScoped
+@Transactional
 public class ExcelService {
+    public ResponseDTO uploadExcel(InputStream req){
+        try {
+            Workbook workbook = new XSSFWorkbook(req);
+            Sheet sheet = workbook.getSheetAt(0);
+
+            List<ExcelResponseDTO> dataList = new ArrayList<>();
+            for (Row row : sheet) {
+                if (row.getRowNum() == 0) { // Skip header row
+                    continue;
+                }
+                ExcelResponseDTO dataMap = new ExcelResponseDTO();
+                dataMap.setId((int) row.getCell(0).getNumericCellValue());
+                dataMap.setName(row.getCell(1).getStringCellValue());
+                dataMap.setGender(row.getCell(2).getStringCellValue());
+                dataMap.setCountry(row.getCell(3).getStringCellValue());
+                dataList.add(dataMap);
+            }
+
+            return new ResponseDTO<>().successResponse(dataList,"Data has been successfully encrypted");
+        } catch (Exception e) {
+            return new ResponseDTO<>().errorResponse(204,"File processing error");
+        }
+    }
 }
