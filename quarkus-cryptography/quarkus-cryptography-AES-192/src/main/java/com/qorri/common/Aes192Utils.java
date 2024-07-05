6747f0526fcbd6d4b3783a1008a227fb1e54b2b4
MrR007 <qorri24j@gmail.com>
2024-07-05 08:58:25 +0700

    add quarkus-cryptography-AES-192

---
+++ quarkus-cryptography/quarkus-cryptography-AES-192/src/main/java/com/qorri/common/Aes192Utils.java
@@ -0,0 +1,29 @@
+package com.qorri.common;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import javax.enterprise.context.ApplicationScoped;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@ApplicationScoped
+public class Aes192Utils {
+    private String AES_ALGORITHM = "AES";
+    private String AES_KEY = "Instagram: @qorri_di AES"; //your-secret-key => aes192 = 24char
+
+    public String encrypt(String plaintext) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(AES_KEY.getBytes(StandardCharsets.UTF_8), AES_ALGORITHM);
+        Cipher cipher = Cipher.getInstance(AES_ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedBytes = cipher.doFinal(plaintext.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    public String decrypt(String encryptedText) throws Exception {
+        SecretKeySpec secretKey = new SecretKeySpec(AES_KEY.getBytes(StandardCharsets.UTF_8), AES_ALGORITHM);
+        Cipher cipher = Cipher.getInstance(AES_ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+        return new String(decryptedBytes, StandardCharsets.UTF_8);
+    }
+}
---
+++ quarkus-cryptography/quarkus-cryptography-AES-192/src/main/java/com/qorri/controller/AES192Controller.java
@@ -0,0 +1,44 @@
+package com.qorri.controller;
+
+import com.qorri.common.Aes192Utils;
+import com.qorri.dto.request.aesRequest;
+import com.qorri.dto.response.*;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+
+@Path("/api/learning/qorri-di/aes-192")
+public class AES192Controller {
+
+    @Inject
+    Aes192Utils aes128;
+
+    @POST
+    @Path("/encrypt")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseDTO aesEncrypt(aesRequest req) throws Exception {
+        if (req == null && req.getInput().isEmpty() && req.getInput().isBlank() && req.getInput().equalsIgnoreCase("")) {
+            return new ResponseDTO<>().errorResponse(203,"Please input value");
+        }
+        aesResponse res = new aesResponse();
+        res.setInput(req.getInput());
+        res.setOutput(aes128.encrypt(req.getInput()));
+        return new ResponseDTO<>().successResponse(res,"Data has been successfully encrypted");
+    }
+
+    @POST
+    @Path("/decrypt")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public ResponseDTO aesDecrypt(aesRequest req) throws Exception {
+        if (req == null && req.getInput().isEmpty() && req.getInput().isBlank() && req.getInput().equalsIgnoreCase("")) {
+            return new ResponseDTO<>().errorResponse(203,"Please input value");
+        }
+        aesResponse res = new aesResponse();
+        res.setInput(req.getInput());
+        res.setOutput(aes128.decrypt(req.getInput()));
+        return new ResponseDTO<>().successResponse(res,"Data has been successfully decrypted");
+    }
+}
